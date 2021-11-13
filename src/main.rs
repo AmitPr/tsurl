82309c614bc01ed4f9461388ef8f9ce49b379bb3
Ananya Sehgal <asehgal4@illinois.edu>
2021-11-13 00:57:15 +0000

    Random alpha generator generates extension

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 #[macro_use] extern crate rocket;
+use rand::{Rng, distributions::Alphanumeric};
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    randomize()
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+fn randomize() -> String {
+    let extension: String = rand::thread_rng().sample_iter(&Alphanumeric).take(8).map(char::from).collect();
+    return extension;
 }
