93be9a5960b5a74cfba43cb92f5e0781494bcb6e
Amit Prasad <amitp4@illinois.edu>
2021-11-12 18:58:05 -0600

    Try redirect

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #[macro_use] extern crate rocket;
+use rocket::response::Redirect;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/test")]
+fn redir() -> Redirect {
+    Redirect::to(uri!("https://google.com"))
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![redir])
 }
