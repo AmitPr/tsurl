10556629ad187de870df78516c7e2025efd7fbd2
Amit Prasad <17amitprasad@gmail.com>
2021-11-18 13:48:17 -0600

    APIError::BadRequest for malformed expiry_times

--- src/api/api_utils.rs
+++ src/api/api_utils.rs
@@ -1,11 +1,17 @@
-use rocket::{Request, http::Status, response::{Responder, content::Json}};
+use rocket::{
+    http::Status,
+    response::{content::Json, Responder},
+    Request,
+};
 /// Error type for API responses. Possible errors are:
 /// - `NotFound`: The requested resource was not found (404).
 /// - `InternalServerError`: An internal server error occurred (500).
+/// - `BadRequest`: The request was malformed (400).
 #[derive(Debug, Clone)]
 pub enum APIError {
     NotFound,
     InternalServerError,
+    BadRequest,
 }
 
 impl APIError {
@@ -14,6 +20,7 @@ impl APIError {
         match self {
             APIError::NotFound => Status::NotFound,
             APIError::InternalServerError => Status::InternalServerError,
+            APIError::BadRequest => Status::BadRequest,
         }
     }
 }
@@ -22,8 +29,21 @@ impl APIError {
 impl<'r> Responder<'r, 'static> for APIError {
     fn respond_to(self, req: &'r Request<'_>) -> rocket::response::Result<'static> {
         let json_string = match self {
-            APIError::NotFound => format!("{{\"error\":{{\"code\":{},\"message\":\"{}\"}}}}", Status::NotFound.code, Status::NotFound.reason().unwrap()),
-            APIError::InternalServerError => format!("{{\"error\":{{\"code\":{},\"message\":\"{}\"}}}}", Status::InternalServerError.code, Status::InternalServerError.reason().unwrap()),
+            APIError::NotFound => format!(
+                "{{\"error\":{{\"code\":{},\"message\":\"{}\"}}}}",
+                Status::NotFound.code,
+                Status::NotFound.reason().unwrap()
+            ),
+            APIError::InternalServerError => format!(
+                "{{\"error\":{{\"code\":{},\"message\":\"{}\"}}}}",
+                Status::InternalServerError.code,
+                Status::InternalServerError.reason().unwrap()
+            ),
+            APIError::BadRequest => format!(
+                "{{\"error\":{{\"code\":{},\"message\":\"{}\"}}}}",
+                Status::BadRequest.code,
+                Status::BadRequest.reason().unwrap()
+            ),
         };
         Json(json_string).respond_to(&req)
     }
