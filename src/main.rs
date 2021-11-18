81d3873efc9a8d7aed3cd36a8ae29d1c5ddbf21c
Amit Prasad <17amitprasad@gmail.com>
2021-11-18 16:24:35 -0600

    Error and Landing Page

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
 #[macro_use]
 extern crate rocket;
+use rocket_dyn_templates::Template;
 
 use database::URL;
+use frontend::frontend_utils::FrontendError;
 use rand::{distributions::Alphanumeric, Rng};
-use rocket::{http::Status, Build, Rocket, State};
+use rocket::{fs::FileServer, Build, Rocket, State};
 use sled_extensions::DbExt;
 
 mod api;
 mod database;
+mod frontend;
 
 /// Catches all shortlinks and redirects to the original URL.
 /// If the shortlink is not found, forwards the APIError status code.
 #[get("/<code>")]
-fn redir(db: &State<database::DB>, code: String) -> Result<URL, Status> {
+fn redir(db: &State<database::DB>, code: String) -> Result<URL, FrontendError> {
     let url = db.get_url(&code);
     match url {
         Ok(url) => Ok(url),
-        Err(err) => Err(err.status()),
+        Err(err) => Err(FrontendError::APIError(err)),
     }
-}
-
-/// Landing page for the service.
-#[get("/")]
-fn index() -> String {
-    randomize()
 }
 
 /// Entrypoint for Rocket.
@@ -35,12 +32,15 @@ fn rocket() -> Rocket<Build> {
         .open()
         .expect("Failed to open sled db");
     rocket::build()
+        .mount("/static", FileServer::from("static"))
+        .mount("/", FileServer::from("static/index").rank(30))
+        .attach(Template::fairing())
         .manage(database::DB {
             urls: db
                 .open_bincode_tree("urls")
                 .expect("Failed to open URL store"),
         })
-        .mount("/", routes![index, redir])
+        .mount("/", routes![redir])
         .mount(
             "/api",
             routes![
