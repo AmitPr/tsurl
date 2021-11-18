2d1f69e9e21376bd63a8e25122d7f71c1c64e924
Ananya Sehgal <asehgal4@illinois.edu>
2021-11-18 18:00:50 +0000

    Checking for expiration time and deleting link. Also created delete_link

--- src/database.rs
+++ src/database.rs
@@ -1,5 +1,6 @@
-use core::time;
-use std::time::{SystemTime, UNIX_EPOCH};
+// use core::time;
+// use std::time::{SystemTime, UNIX_EPOCH};
+extern crate time;
 
 use rocket::{
     http::Status,
@@ -19,8 +20,26 @@ impl DB {
         let url = self.urls.get(&code).unwrap();
         if url.is_none() {
             return Err(APIError::NotFound);
+        } else {
+            let url_reference = url.as_ref().unwrap();
+            if !url_reference.expiry_time.is_none() {
+                let expiration_time = url_reference.expiry_time.unwrap();
+                let system_time = time::get_time();
+                let millisec = system_time.sec + system_time.nsec as i64 / 1000 / 1000;
+                if expiration_time as i64 <= millisec {
+                    self.delete_link(&url.unwrap().code);
+                    return Err(APIError::NotFound);
+                }
+            }
         }
         Ok(url.unwrap().clone())
+    }
+    pub fn delete_link(&self, url: &String) -> Result<URL, APIError> {
+        match self.urls.remove(&url) {
+            Ok(None) => Err(APIError::NotFound),
+            Ok(Some(url)) => Ok(url),
+            Err(_) => Err(APIError::InternalServerError),
+        }
     }
 }
 
@@ -41,3 +60,4 @@ impl<'r> Responder<'r, 'static> for URL {
             .ok()
     }
 }
+
